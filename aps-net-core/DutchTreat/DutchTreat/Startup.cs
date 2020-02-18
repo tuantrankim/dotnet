e13a987ccbe231505b3d59abe10fe74e471db39f
tuantrankim <tuantrankim@users.noreply.github.com>
2020-02-17 23:49:50 -0800

    Add files via upload

---
+++ aps-net-core/DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace DutchTreat
+{
+  public class Startup
+  {
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddIdentity<StoreUser, IdentityRole>(cfg =>
+      {
+        cfg.User.RequireUniqueEmail = true;
+      })
+        .AddEntityFrameworkStores<DutchContext>();
+
+      services.AddAuthentication()
+        .AddCookie()
+        .AddJwtBearer(cfg =>
+        {
+          cfg.TokenValidationParameters = new TokenValidationParameters()
+          {
+            ValidIssuer = _config["Tokens:Issuer"],
+            ValidAudience = _config["Tokens:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]))
+          };
+
+        });
+
+      services.AddDbContext<DutchContext>(cfg =>
+      {
+        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+      });
+
+      services.AddAutoMapper(Assembly.GetEntryAssembly());
+
+      services.AddTransient<DutchSeeder>();
+
+      services.AddScoped<IDutchRepository, DutchRepository>();
+
+      services.AddTransient<IMailService, NullMailService>();
+      // Support for real mail service
+      
+      services.AddControllersWithViews();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseNodeModules();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseRouting();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllerRoute("Default",
+              "{controller}/{action}/{id?}",
+              new { controller = "App", Action = "Index" });
+      });
+
+
+    }
+  }
+}
